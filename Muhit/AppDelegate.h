efb68639589f9e4456058a471219f1768b290c69
Emre YANIK <emre.yanik@bitaksi.com>
2015-06-01 11:20:27 +0300

    Initial Commit

---
+++ Muhit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Muhit
+//
+//  Created by Emre YANIK on 01/06/15.
+//  Copyright (c) 2015 Muhit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Muhit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Muhit
+//
+//  Created by Emre YANIK on 01/06/15.
+//  Copyright (c) 2015 Muhit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
